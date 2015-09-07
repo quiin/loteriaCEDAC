9e3e94d7d2b7d421d70ccb3bca1d1ea4b1c6817a
Carlos Alejandro Reyna González <a01165824@DPDMASTER-89.local>
2015-09-07 17:46:21 -0500

    Initial Commit

---
+++ LoteriaCEDAC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoteriaCEDAC
+//
+//  Created by Carlos Alejandro Reyna González on 07/09/15.
+//  Copyright (c) 2015 CEDAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
