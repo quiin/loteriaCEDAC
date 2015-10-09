4533a7bebbb2a7a44535c73b7eabb1771f9dd770
Diego Monroy Fraustro <a01165792@DPDMASTER-195.local>
2015-10-09 11:00:08 -0500

    mainMenu.swift added pls Gooby

---
+++ LoteriaCEDAC/mainMenu.swift
@@ -0,0 +1,13 @@
+//
+//  mainMenu.swift
+//  LoteriaCEDAC
+//
+//  Created by Diego Monroy Fraustro on 09/10/15.
+//  Copyright (c) 2015 CEDAC. All rights reserved.
+//
+
+import UIKit
+
+class mainMenu: UIViewController {
+    
+}
