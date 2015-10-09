2b9c0e9aa8cbf04ea02fad07ebb4f7d31d6fdb73
Carlos Alejandro Reyna González <a01165824@DPDMASTER-89.local>
2015-10-09 10:27:57 -0500

    Changed to CollectionView in MemoryGameController.swift

--- LoteriaCEDAC/memoryGameController.swift
+++ LoteriaCEDAC/memoryGameController.swift
@@ -8,11 +8,63 @@
 
 import UIKit
 
-class MemoryGameController: UIViewController {
+class MemoryGameController: UIViewController, UICollectionViewDelegate,UICollectionViewDataSource {
 
+    
+    //vars
+    var flipped = [Bool] (count: 18, repeatedValue: false)
+    var cards = ["🐂", "🐎","🐯","🐤","👉","👌","🎁","🎂", "🎃", "🍆","🍕","🍻","🍳","🐸","🐬","🐠","🐝","💦",
+                 "🐂", "🐎","🐯","🐤","👉","👌","🎁","🎂", "🎃", "🍆","🍕","🍻","🍳","🐸","🐬","🐠","🐝","💦"]
+    
+    var isFirstPairOpen = false
+    var firstPairIndex = 0
+    var firstPair = ""
+    var secondPair = ""
+    
+    //constants
+    let backCard = "❓"
+    
+    
+    
+    
+    
+    
+    
+    //Collection methods
+    //items in section
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cards.count
+    }
+    
+    //number of sections
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    //handle item select
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+    
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.cellForItemAtIndexPath(indexPath)
+       return cell!
+    }
+
+    //shuffleFlags
+    func shuffleFlags(){
+        for i in 0..<flipped.count{
+            var j = Int(arc4random())%flipped.count
+            let temp = flipped[i]
+            flipped[i] = flipped[j]
+            flipped[j] = temp
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        shuffleFlags()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +72,6 @@ class MemoryGameController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
 }
 
