59a8d334723dc166c8f6a16f40528642465ff5af
Diego Monroy Fraustro <a01165792@DMDMASTER-59.local>
2015-10-09 12:39:47 -0500

    Added main skeleton/structure for interface.

---
+++ LoteriaCEDAC/playModeMenu_viewControllerViewController.swift
@@ -0,0 +1,36 @@
+//
+//  playModeMenu_viewControllerViewController.swift
+//  LoteriaCEDAC
+//
+//  Created by Diego Monroy Fraustro on 09/10/15.
+//  Copyright (c) 2015 CEDAC. All rights reserved.
+//
+
+import UIKit
+
+class playModeMenu_viewControllerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Modo de juego"
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
